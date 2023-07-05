e3c3ca37b0c045f89ae635d67eb83233e37eb7b2
DaniiLBez <dvbezzubov@stud.eltech.ru>
2023-07-05 13:19:53 +0300

    add fields with distances and path to Dijkstra class

--- src/jvmMain/kotlin/algorithm/Dijkstra.kt
+++ src/jvmMain/kotlin/algorithm/Dijkstra.kt
@@ -5,17 +5,19 @@ import kotlin.math.abs
 
 @Suppress("DEPRECATION")
 class Dijkstra(private val graph: Graph) {
+	private var distanceFromSource: HashMap<Vertex, Visit>? = null
+	private var resultPath: MutableList<Edge>? = null
 	private fun route(
 		destination: String,
 		paths: HashMap<Vertex, Visit>
-	): MutableList<Edge>{
+	): MutableList<Edge> {
 		var vertex = graph.vertex(destination)
 		val path = mutableListOf<Edge>()
-		loop@while(true){
+		loop@while (true) {
 			val visit = paths[vertex] ?: break
 
-			when(visit.type){
-				VisitType.EDGE -> visit.edge?.let{
+			when (visit.type) {
+				VisitType.EDGE -> visit.edge?.let {
 					path.add(it)
 					vertex = it.source
 				}
@@ -28,13 +30,19 @@ class Dijkstra(private val graph: Graph) {
 	private fun distance(
 		destination: String,
 		paths: HashMap<Vertex, Visit>
-	): Double{
+	): Double {
 		val path = route(destination, paths)
-		return path.sumByDouble{ it.weight ?: 0.0 }
+		return path.sumByDouble { it.weight ?: 0.0 }
 	}
 
-	private fun shortestPath(startVertex: String): HashMap<Vertex, Visit>{
-		val start = graph.vertex(startVertex) ?: return HashMap()
+	private fun shortestPath(startVertex: String) {
+		val start = graph.vertex(startVertex)
+
+		if (start == null) {
+			distanceFromSource = HashMap()
+			return
+		}
+
 		val paths = hashMapOf<Vertex, Visit>()
 		paths[start] = Visit(VisitType.START)
 
@@ -44,36 +52,37 @@ class Dijkstra(private val graph: Graph) {
 
 		priorityQueue.add(start)
 
-		while(priorityQueue.isNotEmpty()){
+		while (priorityQueue.isNotEmpty()) {
 			val vertex = priorityQueue.remove()
 			val edges = graph.edges(vertex)
 
-			edges.forEach{
+			edges.forEach {
 				val weight = it.weight ?: return@forEach
 
 				if (paths[it.destination] == null ||
-					distance(vertex.name, paths) + weight < distance(it.destination.name, paths)){
+					distance(vertex.name, paths) + weight < distance(it.destination.name, paths)
+				) {
 					paths[it.destination] = Visit(VisitType.EDGE, it)
 					priorityQueue.add(it.destination)
 				}
 			}
 		}
-		return paths
+		distanceFromSource = paths
 	}
 
 	fun shortestPath(
 		source: String,
 		destination: String
-	): MutableList<Edge>{
-		val paths = shortestPath(source)
-		return route(destination, paths)
+	) {
+		this.shortestPath(source)
+		resultPath = route(destination, distanceFromSource!!)
 	}
-
+	fun getPath() = resultPath
 }
 
 class Visit(val type: VisitType, val edge: Edge? = null)
 
-enum class VisitType{
+enum class VisitType {
 	START,
 	EDGE
 }
