b68cde4555d9b78271f943f413fd7212dfb40990
LeraChernyakova <2215391@mail.ru>
2023-07-06 23:32:57 +0300

    Configuring compatibility with the updated algorithm architecture

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,7 +1,7 @@
 package UI
 
-import algorithm.AdjacencyList
-import algorithm.Graph
+import algorithm.graph.AdjacencyList
+import algorithm.graph.Graph
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
